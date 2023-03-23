25c0e9a069377806c7c977ca707b9ba456f6284d
Estudiandev <henrry.bourgeot@linktic.com>
2023-03-22 21:34:17 -0400

    Added more features

--- src/gui/GUICalc.java
+++ src/gui/GUICalc.java
@@ -5,6 +5,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUICalc extends JFrame{
+    private double num1;
+    private double num2;
+    private String operation;
     private JPanel panel1;
     private JTextField calcText;
     private JButton btn7;
@@ -28,19 +31,200 @@ public class GUICalc extends JFrame{
         btn0.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                calcText.setText("0");
+                if(!operation.equals("")){
+                    calcText.setText("");
+                }
+
+                calcText.setText(calcText.getText() + "0");
             }
         });
         btn1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if(!operation.equals("")){
+                    calcText.setText("");
+                }
+
                 calcText.setText(calcText.getText() + "1");
             }
+        });
+        btn2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!operation.equals("")){
+                    calcText.setText("");
+                }
+
+                calcText.setText(calcText.getText() + "2");
+            }
+        });
+        btn3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!operation.equals("")){
+                    calcText.setText("");
+                }
+
+                calcText.setText(calcText.getText() + "3");
+            }
+        });
+        btn4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!operation.equals("")){
+                    calcText.setText("");
+                }
+                calcText.setText(calcText.getText() + "4");
+            }
+        });
+        btn5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!operation.equals("")){
+                    calcText.setText("");
+                }
+
+                calcText.setText(calcText.getText() + "5");
+            }
+        });
+        btn6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!operation.equals("")){
+                    calcText.setText("");
+                }
+                calcText.setText(calcText.getText() + "6");
+            }
+        });
+        btn7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!operation.equals("")){
+                    calcText.setText("");
+                }
+                calcText.setText(calcText.getText() + "7");
+            }
+        });
+        btn8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!operation.equals("")){
+                    calcText.setText("");
+                }
+                calcText.setText(calcText.getText() + "8");
+            }
+        });
+        btn9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!operation.equals("")){
+                    calcText.setText("");
+                }
+                calcText.setText(calcText.getText() + "9");
+            }
+        });
+        btnDecimal.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!calcText.getText().contains(".")) {
+                    calcText.setText(calcText.getText() + ".");
+                }
+            }
+        });
+        btnTimes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+                if(num1==0){
+                    num1 = Double.parseDouble(calcText.getText());
+                    calcText.setText("");
+                }else if(num2!=0 && operation.equals("nothing")){
+                    num1 = Double.parseDouble(calcText.getText());
+                    calcText.setText("");
+                    num2=0;
+                } else{
+                    num1=num1 * num2;
+                    num2 = Double.parseDouble(calcText.getText());
+                    calcText.setText(String.valueOf(num1 * num2));
+                }
+                operation = "multi";
+            }
+        });
+        btnDivide.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(num1==0){
+                    num1 = Double.parseDouble(calcText.getText());
+                    calcText.setText("");
+                }else if(num2!=0 && operation.equals("nothing")){
+                    num1 = Double.parseDouble(calcText.getText());
+                    calcText.setText("");
+                    num2=0;
+                } else{
+                    num1=num1 / num2;
+                    num2 = Double.parseDouble(calcText.getText());
+                    calcText.setText(String.valueOf(num1 / num2));
+                }
+                operation = "conquer";
+            }
+        });
+        btnPlus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(num1==0){
+                    num1 = Double.parseDouble(calcText.getText());
+                    calcText.setText("");
+                }else if(num2!=0 && operation.equals("nothing")){
+                    num1 = Double.parseDouble(calcText.getText());
+                    calcText.setText("");
+                    num2=0;
+                } else{
+                    System.out.println(num1);
+                    num1=num1 + num2;
+                    num2 = Double.parseDouble(calcText.getText());
+                    calcText.setText(String.valueOf(num1 + num2));
+                }
+                operation = "power";
+            }
+        });
+        btnMinus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(num1==0){
+                    num1 = Double.parseDouble(calcText.getText());
+                    calcText.setText("");
+                }else if(num2!=0 && operation.equals("nothing")){
+                    num1 = Double.parseDouble(calcText.getText());
+                    calcText.setText("");
+                    num2=0;
+                } else{
+                    System.out.println(num1);
+                    num1=num1 * num2;
+                    num2 = Double.parseDouble(calcText.getText());
+                    calcText.setText(String.valueOf(num1 * num2));
+                }
+                operation = "linus";
+            }
+        });
+        btnEqual.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    num2 = Double.parseDouble(calcText.getText());
+                switch (operation) {
+                    case "multi" -> calcText.setText(String.valueOf(num1 * num2));
+                    case "conquer" -> calcText.setText(String.valueOf(num1 / num2));
+                    case "power" -> calcText.setText(String.valueOf(num1 + num2));
+                    case "linus" -> calcText.setText(String.valueOf(num1 - num2));
+                    default -> calcText.setText("NO OPERATION, SELECT ONE");
+                }
+                operation = "nothing";
+                }
         });
     }
 
     private void createUIComponents() {
-        // TODO: place custom component creation code here
+
     }
 
     public static void main(String []args){
@@ -48,6 +232,9 @@ public class GUICalc extends JFrame{
         calc.setContentPane(calc.panel1);
         calc.setTitle("A java calculator");
         calc.setSize(230,350);
+
+        // here's the part where i center the jframe on screen
+        calc.setLocationRelativeTo(null);
         calc.setVisible(true);
         calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
